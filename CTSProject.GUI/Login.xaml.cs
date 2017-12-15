06fc93432e1e6de90f74d68638ebf0ca63afedfa
Анастасия Степура <stepura99@mail.ru>
2017-12-15 18:25:25 +0300

    Добавлены макеты GUI окон

---
+++ CTSProject.GUI/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CTSProject.GUI
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            //if (LoginBox.Text = УНИКАЛЬНОСТЬ ПОИСКОМ ПО БД)
+            // ЗАПИСАТЬ ЛОГИН В БД
+            Close();
+            Owner.IsHitTestVisible = true;
+        }
+    }
+}
